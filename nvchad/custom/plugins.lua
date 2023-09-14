b5de7439173376cd021c74dd5e0c8b4896d9fc9f
tw <tw@tw>
2023-09-14 08:52:52 +0600

    Add docker lsp config

--- nvchad/custom/plugins.lua
+++ nvchad/custom/plugins.lua
@@ -2,6 +2,8 @@ require'lspconfig'.rust_analyzer.setup{}
 require'lspconfig'.intelephense.setup{}
 require'lspconfig'.zls.setup{}
 require'lspconfig'.gopls.setup{}
+require'lspconfig'.docker_compose_language_service.setup{}
+require'lspconfig'.dockerls.setup{}
 
 require("base46").toggle_transparency()
 
