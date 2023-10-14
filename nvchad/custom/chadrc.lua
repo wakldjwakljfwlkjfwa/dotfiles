4feec72db759c520aad90d850345601ad70defb2
tw <tw@tw>
2023-10-14 20:25:12 +0600

    Add keymap

--- nvchad/custom/chadrc.lua
+++ nvchad/custom/chadrc.lua
@@ -1,6 +1,7 @@
 vim.api.nvim_set_keymap('n', '<leader>lk', [[<Cmd>lua vim.lsp.buf.hover()<CR>]], { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>la', [[<Cmd>lua vim.lsp.buf.code_action()<CR>]], { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>lo', [[<Cmd>lua vim.diagnostic.open_float()<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>lr', [[<Cmd>lua vim.lsp.buf.rename()<CR>]], { noremap = true, silent = true })
 
 local M = {}
 M.ui = {theme = 'catppuccin'}
