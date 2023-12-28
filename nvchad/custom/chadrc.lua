448042f6886c2513db7d71e172a9af3aebae7138
tw <tw@tw>
2023-12-28 09:25:18 +0600

    Update nvchad

--- nvchad/custom/chadrc.lua
+++ nvchad/custom/chadrc.lua
@@ -2,6 +2,7 @@ vim.api.nvim_set_keymap('n', '<leader>lk', [[<Cmd>lua vim.lsp.buf.hover()<CR>]],
 vim.api.nvim_set_keymap('n', '<leader>la', [[<Cmd>lua vim.lsp.buf.code_action()<CR>]], { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>lo', [[<Cmd>lua vim.diagnostic.open_float()<CR>]], { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>lr', [[<Cmd>lua vim.lsp.buf.rename()<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>lf', [[<Cmd>lua vim.lsp.buf.references()<CR>]], { noremap = true, silent = true })
 
 local M = {}
 M.ui = {theme = 'catppuccin'}
