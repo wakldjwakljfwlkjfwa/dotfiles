b3b848dafe03efce9889d8a6748ee5535fb2c854
tw <tw@tw>
2024-01-18 14:30:08 +0600

    Update nvchad config

--- nvchad/custom/plugins.lua
+++ nvchad/custom/plugins.lua
@@ -1,29 +1,33 @@
-require'lspconfig'.rust_analyzer.setup{}
-require'lspconfig'.intelephense.setup{}
-require'lspconfig'.zls.setup{}
-require'lspconfig'.gopls.setup{
+require 'lspconfig'.rust_analyzer.setup {}
+require 'lspconfig'.intelephense.setup {}
+require 'lspconfig'.zls.setup {}
+require 'lspconfig'.gopls.setup {
   settings = {
     gopls = {
       gofumpt = true
     }
   }
 }
-require'lspconfig'.docker_compose_language_service.setup{
+require 'lspconfig'.docker_compose_language_service.setup {
   filetypes = { "yaml", "yml" }
 }
-require'lspconfig'.dockerls.setup{}
-require'lspconfig'.clangd.setup{}
-require'lspconfig'.pylsp.setup{}
-require'lspconfig'.dartls.setup{}
+require 'lspconfig'.dockerls.setup {}
+require 'lspconfig'.clangd.setup {}
+require 'lspconfig'.pylsp.setup {}
+require 'lspconfig'.dartls.setup {}
 
 require("base46").toggle_transparency()
 
 local plugins = {
   {
     "cappyzawa/trim.nvim",
-    config = function ()
+    config = function()
       require("trim").setup({})
     end,
+    lazy = false
+  },
+  {
+    "mbbill/undotree",
     lazy = false
   }
 }
