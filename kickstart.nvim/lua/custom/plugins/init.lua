ea5dbde1ef1168390a8ddc92240e93ebb68fe69d
tw <tw@tw>
2024-01-28 09:37:28 +0600

    Update kickstart.nvim

--- kickstart.nvim/lua/custom/plugins/init.lua
+++ kickstart.nvim/lua/custom/plugins/init.lua
@@ -2,4 +2,14 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+
+vim.opt.rnu = true
+
+vim.api.nvim_set_keymap('n', '<leader>lk', [[<Cmd>lua vim.lsp.buf.hover()<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>la', [[<Cmd>lua vim.lsp.buf.code_action()<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>lo', [[<Cmd>lua vim.diagnostic.open_float()<CR>]],
+  { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>lr', [[<Cmd>lua vim.lsp.buf.rename()<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>lf', [[<Cmd>lua vim.lsp.buf.references()<CR>]], { noremap = true, silent = true })
+
 return {}
